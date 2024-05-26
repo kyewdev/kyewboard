9d72ead89413e322c704d9a50286191e23fa2e58
Kyew <qlauterbach.dev@gmail.com>
2024-05-26 20:51:19 +0200

    Add build file

---
+++ build.bat
@@ -0,0 +1,14 @@
+@echo off
+setlocal
+
+if "%1" == "templ" (
+    start cmd /k "templ generate -watch -proxy=http://localhost:42069"
+) else if "%1" == "air" (
+    start cmd /k "air"
+) else if "%1" == "tailwind" (
+    start cmd /k "npx tailwindcss -i .\pkg\view\app.css -o .\assets\style.css --watch"
+) else (
+    echo Invalid argument. Please use templ, air, or tailwind.
+)
+
+endlocal
